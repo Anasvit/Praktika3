a6e63344c658c4252bd3e3b690eaa302a0326de3
GuselnikovaAV <GuselnikovaAV@310D-9>
2024-12-05 09:40:44 +0700

    Добавьте файлы проекта.

---
+++ Praktika3/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(DigitalRoot(16));
+        Console.WriteLine(DigitalRoot(942));
+
+        static int DigitalRoot(int num)
+        {
+            while (num >= 10)
+            {
+                int sum = 0;
+
+                foreach (char digit in num.ToString())
+                {
+                    sum += (digit - '0');
+                }
+
+                num = sum;
+            }
+
+            return num;
+        }
+    }
+}
